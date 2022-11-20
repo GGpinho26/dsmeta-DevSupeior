576058465fc1c178965cdb75a1d13de28971cb1c
GGpinho26 <ggpinho26@gmail.com>
2022-11-20 11:15:06 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notific-icon.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div>
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
